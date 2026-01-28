9bdd17bd40f7863a2b44edd1cd51b1a9149a3267
ecastisu <ecastisu@emeal.nttdata.com>
2026-01-28 15:43:38 -0500

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mi-org-terraform-state-123456"
-    key            = "s3-demo/dev/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
